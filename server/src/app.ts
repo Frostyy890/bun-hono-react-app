b5b0efe55ce53ddb7bc07976995415c10e4c3978
Frostyy890 <abdurahmon2004nuriy@gmail.com>
2024-08-10 18:56:59 +0500

    refactor api routing create type for rpc api routes

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,7 +1,7 @@
 import { Hono } from "hono";
 import { logger } from "hono/logger";
 import settings from "./config/settings";
-import appRoutes from "./api/v1/routes/AppRoutes";
+import appRoutes_v1 from "./api/v1/routes/AppRoutes";
 import errorHandler from "./api/v1/middlewares/ErrorHandler";
 import { serveStatic } from "hono/bun";
 
@@ -13,7 +13,7 @@ app.onError((err, c) => {
   return c.json({ message }, { status });
 });
 
-app.route("/api/v1", appRoutes);
+const apiRoutes = app.basePath("/api").route("/v1", appRoutes_v1);
 
 // Server client code !For production
 app.get("*", serveStatic({ root: "./client/dist" }));
@@ -23,3 +23,5 @@ Bun.serve({
   port: settings.port,
   fetch: app.fetch,
 });
+
+export type ApiRoutes = typeof apiRoutes;
