64938023d5c5a60ec99faeb278aa797a1294eb24
Abdurakhmon Nuriy <abdurahmon2004nuriy@gmail.com>
2024-08-12 16:24:54 +0500

    refactor errorHandler factory

--- server/src/api/v1/middlewares/ErrorHandler.ts
+++ server/src/api/v1/middlewares/ErrorHandler.ts
@@ -1,5 +1,5 @@
 import type { HTTPResponseError } from "hono/types";
-import postgres from "postgres";
+import postgres, { PostgresError } from "postgres";
 import { HTTPException } from "hono/http-exception";
 import type { StatusCode } from "hono/utils/http-status";
 import HTTPStatusCode from "../constants/HTTPStatusCode";
@@ -8,11 +8,20 @@ export default function errorHandler(err: Error | HTTPResponseError): {
   message: string;
   status: StatusCode;
 } {
-  if (err instanceof postgres.PostgresError) {
-    if (err.code === "23505" && err.detail)
-      return { message: err.detail, status: HTTPStatusCode.CONFLICT };
-    return { message: "Internal Server Error", status: HTTPStatusCode.INTERNAL_SERVER_ERROR };
-  }
-  if (err instanceof HTTPException) return { message: err.message, status: err.status };
+  if (err instanceof postgres.PostgresError) return handlePostgresError(err);
+
+  if (err instanceof HTTPException) return handleHTTPException(err);
+
+  console.error(`Unhandled exception`, JSON.stringify(err, null, 2));
   return { message: "Internal Server Error", status: HTTPStatusCode.INTERNAL_SERVER_ERROR };
+}
+
+function handlePostgresError(error: PostgresError) {
+  if (error.code === "23505" && error.detail)
+    return { message: error.detail, status: HTTPStatusCode.CONFLICT };
+  return { message: "Internal Server Error", status: HTTPStatusCode.INTERNAL_SERVER_ERROR };
+}
+
+function handleHTTPException(error: HTTPException) {
+  return { message: error.message, status: error.status };
 }
