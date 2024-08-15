bded62e0abaef457692df8a3aa6b843b26a1269f
Abdurakhmon Nuriy <abdurahmon2004nuriy@gmail.com>
2024-08-15 15:27:53 +0500

    updated errorHandler to handle base errors and their edge cases

--- server/src/api/v1/middlewares/ErrorHandler.ts
+++ server/src/api/v1/middlewares/ErrorHandler.ts
@@ -1,5 +1,5 @@
 import type { HTTPResponseError } from "hono/types";
-import postgres, { PostgresError } from "postgres";
+import postgres from "postgres";
 import { HTTPException } from "hono/http-exception";
 import type { StatusCode } from "hono/utils/http-status";
 import HTTPStatusCode from "../constants/HTTPStatusCode";
@@ -12,11 +12,13 @@ export default function errorHandler(err: Error | HTTPResponseError): {
 
   if (err instanceof HTTPException) return handleHTTPException(err);
 
+  if (err instanceof Error) return handleBaseError(err);
+
   console.error(`Unhandled exception`, JSON.stringify(err, null, 2));
   return { message: "Internal Server Error", status: HTTPStatusCode.INTERNAL_SERVER_ERROR };
 }
 
-function handlePostgresError(error: PostgresError) {
+function handlePostgresError(error: postgres.PostgresError) {
   if (error.code === "23505" && error.detail)
     return { message: error.detail, status: HTTPStatusCode.CONFLICT };
   return { message: "Internal Server Error", status: HTTPStatusCode.INTERNAL_SERVER_ERROR };
@@ -24,4 +26,11 @@ function handlePostgresError(error: PostgresError) {
 
 function handleHTTPException(error: HTTPException) {
   return { message: error.message, status: error.status };
+}
+
+function handleBaseError(error: Error) {
+  if (error.message === "No values to set")
+    return { message: error.message, status: HTTPStatusCode.BAD_REQUEST };
+  console.error("Unhandled Base Error", JSON.stringify(error, null, 2));
+  return { message: "Internal Server Error", status: HTTPStatusCode.INTERNAL_SERVER_ERROR };
 }
